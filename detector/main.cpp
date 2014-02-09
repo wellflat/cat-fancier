e0be1edc7a73e251efc0ee934097ee21abd3d9da
wellflat <wellflat@gmail.com>
2014-02-09 15:20:53 +0900

    added object detector test code

---
+++ detector/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+int main(int argc, char** argv) {
+  using namespace std;
+  cv::Mat src_img = cv::imread(argv[1], 1);
+  string cascade_file = "./" + string(argv[2]);
+  cv::CascadeClassifier cascade;
+  cout << "cascade file: " << cascade_file << endl;
+  cascade.load(cascade_file);
+  if(cascade.empty()) {
+    cerr << "cannot load cascade file" << endl;
+    exit(-1);
+  }
+  vector<cv::Rect> objects;
+  cascade.detectMultiScale(src_img, objects);
+  vector<cv::Rect>::const_iterator iter = objects.begin();
+  cout << "count: " << objects.size() << endl;
+  while(iter!=objects.end()) {
+    cv::rectangle(src_img,
+                  cv::Rect(iter->x, iter->y, iter->width, iter->height),
+                  cv::Scalar(0, 255, 0));
+    ++iter;
+  }
+  cv::imwrite("box/detect.jpg", src_img);
+  return 0;
+}
