707e99b6512a8476b5a9f6d7e93f7872c8d841ce
ryo <wellflat@gmail.com>
2014-02-09 15:51:38 +0900

    modified flickr_downloader.py

--- tools/clipper/bin/flickr_downloader.py
+++ tools/clipper/bin/flickr_downloader.py
@@ -3,8 +3,19 @@
 
 import urllib2
 import json
+import optparse
 import sys
 from pprint import pprint
+
+def createparser():
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--download', action='store_true', dest='download')
+    parser.add_option('-t', '--tag', dest='tag')
+    parser.add_option('-p', '--page', dest='maxpage', type='int', default=10)
+    parser.add_option('-c', '--cconly', action='store_true', dest='cconly', default=False)
+    parser.add_option('-m', '--mine', action='store_true', dest='mine', default=False)
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False)
+    return parser
 
 class FlickrClient(object):
     
@@ -53,28 +64,36 @@ class FlickrClient(object):
             
 if __name__ == '__main__':
     try:
+        parser = createparser()
+        (options, args) = parser.parse_args()
         apikey = 'cf34b6d0fc8d5d2924ba67c7158739a3'
         apisecret = 'eca90abc0e259384'
         userid = '95962563@N02'
         flickr = FlickrClient(apikey, apisecret, userid)
-        if len(sys.argv) == 2:
+        if options.tag:
             fsencoding = sys.getfilesystemencoding() or sys.getdefaultencoding()
-            tag = sys.argv[1].decode(fsencoding)
+            tag = options.tag.decode(fsencoding)
         else:
             tag = u'cat'
         print('search by %s' % (tag,))
-        dstdir = './static/images/tmp'
+        print('creative commons only: %s' % (options.cconly,))
+        print('my photo only: %s' % (options.mine,))
+        #dstdir = './static/images/tmp'
+        dstdir = './static/negative/images/other'
         print('save to: %s' % (dstdir,))
-        ret = flickr.getbytag(tag, ismine=True, cconly=True)
+        ret = flickr.getbytag(tag, ismine=options.mine, cconly=options.cconly)
         pagenum = int(ret['photos']['pages'])
+        if pagenum > options.maxpage:
+            pagenum = options.maxpage
         print('total pages: %s' % (pagenum,))
         total = ret['photos']['total']
         print('total photos: %s' % (total,))
         firstpage = 1
         for i in xrange(firstpage, pagenum + 1):
             print('page %s' % (i,))
-            ret = flickr.getbytag(tag, page=i, ismine=True, cconly=True)
-            flickr.downloadphotos(ret['photos']['photo'], dstdir, verbose=True)
+            ret = flickr.getbytag(tag, page=i, ismine=options.mine, cconly=options.cconly)
+            if options.download:
+                flickr.downloadphotos(ret['photos']['photo'], dstdir, verbose=True)
             
         print('complete.')
     except KeyboardInterrupt as e:
