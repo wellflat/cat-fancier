77fabc26b544c628887ad56a6c6f40d60989224c
wellflat <wellflat@gmail.com>
2014-02-15 23:36:11 +0900

    migrated optparse to argparse

--- detector/objdetect.py
+++ detector/objdetect.py
@@ -1,25 +1,29 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
 import sys
 import cv2 as cv
 
-def detect(filename, cascadefilename='cascade.xml'):
-    srcimg = cv.imread(filename)
+def parsearguments():
+    parser = argparse.ArgumentParser(description='object detection using cascade classifier')
+    parser.add_argument('imagefilename', help='image file name')
+    parser.add_argument('cascadefilename', help='cascade file name')
+    parser.add_argument('-o', '--output', dest='output')
+    return parser.parse_args()
+
+def detect(imagefilename, cascadefilename):
+    srcimg = cv.imread(imagefilename)
     cascade = cv.CascadeClassifier(cascadefilename)
-    objects = cascade.detectMultiScale(srcimg, 10.1, 3)
+    objects = cascade.detectMultiScale(srcimg, 1.1, 3)
     for (x, y, w, h) in objects:
         print(x, y, w, h)
         cv.rectangle(srcimg, (x, y), (x + w, y + h), (0, 0, 255), 4)
     return srcimg
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('./objdetect [image file] [cascade file]')
-        sys.exit(-1)
-    filename = sys.argv[1]
-    cascadefilename = sys.argv[2]
-    result = detect(filename, cascadefilename)
+    args = parsearguments()
+    result = detect(args.imagefilename, args.cascadefilename)
     cv.imwrite('box/detect.jpg', result)
     
     
