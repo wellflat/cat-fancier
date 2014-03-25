c0bf0a32d834baaad921882f5d7d8473df3bffc3
ryo <wellflat@gmail.com>
2014-03-25 13:38:10 +0900

    update cascade model

--- tools/clipper/bin/train_cascade.sh
+++ tools/clipper/bin/train_cascade.sh
@@ -4,3 +4,4 @@
 #    /usr/local/bin/opencv_createsamples -vec yahoo_88_24.vec -img static/images/yahoo/yahoo-japan.jpg -bg negative.dat -num 2000 -bgcolor 255 -w 88 -h 24
 
 time opencv_traincascade -data $DST_DIR -vec $VEC_FILE -bg $BG_FILE -numPos $1 -numNeg $2 -featureType LBP -maxFalseAlarmRate 0.4 > train.log
+time opencv_traincascade -data $DST_DIR -vec $VEC_FILE -bg $BG_FILE -numPos $1 -numNeg $2 -featureType LBP -w 88 -h 24 -maxFalseAlarmRate 0.4 > train.log
